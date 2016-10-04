6a7328992f7abb621ccd85690ab9969d02a21cb8
Diya Zhao <zdy.gogo@msn.com>
2016-10-04 19:27:36 +0000

    change config.js server route

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 
                         global.DATABASE_URL || 
                         (process.env.NODE_ENV == 'production' ?
-                        'mongodb://localhost/shopping-list' :
+                        'mongodb://diya:123@ds049436.mlab.com:49436/mongo-shopping-list':
                             'mongodb://localhost/shopping-list-dev');
                             
 exports.PORT = process.env.PORT || 8080;
